654eb9fe5503ea47b4815e3e81a8c899a6df8842
Duane Robertson <duane@duanerobertson.com>
2016-03-01 04:59:03 -0600

    Correct gaps in terrain.

--- mapgen.lua
+++ mapgen.lua
@@ -73,18 +73,20 @@ function cityscape.generate(minp, maxp, seed)
 
 	local index = 0
 	local avg = 0
-	local avg_a = {{0,0,0}, {0,0,0}, {0,0,0}}
-	local edge_y = {}
-	local corner = {}
+	local count = 0
 	local min = 31000
 	local max = -31000
-	local outs = 0
+	local outs_up = 0
+	local outs_down = 0
 	for z = minp.z, maxp.z do
 		for x = minp.x, maxp.x do
 			index = index + 1
-			if heightmap[index] > maxp.y or heightmap[index] < minp.y then
-				outs = outs + 1
+			if heightmap[index] > maxp.y then
+				outs_up = outs_up + 1
+			elseif heightmap[index] < minp.y then
+				outs_down = outs_down + 1
 			end
+
 			if heightmap[index] < min then
 				min = heightmap[index]
 			end
@@ -95,54 +97,23 @@ function cityscape.generate(minp, maxp, seed)
 				return
 			end
 
-			--avg_a[2][2] = avg_a[2][2] + heightmap[index]
-			--if x == minp.x then
-			--	avg_a[1][2] = avg_a[1][2] + heightmap[index]
-			--end
-			--if x == maxp.x then
-			--	avg_a[3][2] = avg_a[3][2] + heightmap[index]
-			--end
-			--if z == minp.z then
-			--	avg_a[2][1] = avg_a[2][1] + heightmap[index]
-			--end
-			--if z == maxp.z then
-			--	avg_a[2][3] = avg_a[2][3] + heightmap[index]
-			--end
-			--if max < heightmap[index] then
-			--	max = heightmap[index]
-			--end
-			--if min > heightmap[index] then
-			--	min = heightmap[index]
-			--end
+			avg = avg + heightmap[index]
+			count = count + 1
 		end
 	end
-	--avg_a[2][2] = avg_a[2][2] / (csize.x * csize.z)
-	--avg_a[1][2] = avg_a[1][2] / (csize.x + csize.z)
-	--avg_a[3][2] = avg_a[3][2] / (csize.x + csize.z)
-	--avg_a[2][1] = avg_a[2][1] / (csize.x + csize.z)
-	--avg_a[2][3] = avg_a[2][3] / (csize.x + csize.z)
 
-	--avg_a[1][1] = (avg_a[1][2] + avg_a[2][1]) / 2
-	--avg_a[1][3] = (avg_a[1][2] + avg_a[2][3]) / 2
-	--avg_a[3][1] = (avg_a[2][1] + avg_a[3][2]) / 2
-	--avg_a[3][3] = (avg_a[2][3] + avg_a[3][2]) / 2
-
-	--corner[1] = avg_a[1][1]
-	--corner[2] = avg_a[3][1]
-	--corner[3] = avg_a[1][3]
-	--corner[4] = avg_a[3][3]
-	--avg = avg_a[2][2]
-
-	corner[1] = math.max(math.min(maxp.y, heightmap[1]), minp.y)  -- x0, z0
-	corner[2] = math.max(math.min(maxp.y, heightmap[maxp.x - minp.x + 1]), minp.y)  -- x1, z0
-	corner[3] = math.max(math.min(maxp.y, heightmap[(maxp.z - minp.z) * (maxp.x - minp.x + 1) + 1]), minp.y)  -- x0, z1
-	corner[4] = math.max(math.min(maxp.y, heightmap[(maxp.z - minp.z + 1) * (maxp.x - minp.x + 1)]), minp.y)  -- x1, z1
-	avg = math.floor((corner[1] + corner[2] + corner[3] + corner[4]) / 4 + 0.5)
-	if avg > minp.y + 60 then
-		--return
+	avg = math.floor(avg / count + 0.5)
+	if avg > minp.y + 67 then
+		return
 	end
 
-	local airy = outs > csize.x * csize.z / 2
+	local airy = outs_down > 0 and outs_up + outs_down < csize.x * csize.z
+	local earthy = outs_up > 0 and outs_up + outs_down < csize.x * csize.z
+	local urban = outs_up + outs_down < csize.x * csize.z / 2
+	local centered = avg >= minp.y and avg <= maxp.y
+	local streetw = 5
+	local sidewalk = 2
+
 	local bh = {}
 	for i = 1,3 do
 		bh[i] = {}
@@ -155,31 +126,30 @@ function cityscape.generate(minp, maxp, seed)
 	local rx = csize.x / 3 + 0.01
 	local rz = csize.z / 3 + 0.01
 
-	--if max - min <= 20 then
 	if true then
 		for z = minp.z, maxp.z do
 			for x = minp.x, maxp.x do
 				local ivm = a:index(x, minp.y, z)
-				edge_y[1] = (corner[2] - corner[1]) * ((x - minp.x) / csize.x) + corner[1]
-				edge_y[3] = (corner[3] - corner[1]) * ((z - minp.z) / csize.z) + corner[1]
-				edge_y[2] = (corner[4] - corner[3]) * ((x - minp.x) / csize.x) + corner[3]
-				edge_y[4] = (corner[4] - corner[2]) * ((z - minp.z) / csize.z) + corner[1]
 				for y = minp.y, maxp.y do
-					if airy then
+					if airy and urban and not centered then
 						data[ivm] = node["air"]
-					else
+					elseif earthy and urban and not centered then
+						data[ivm] = node["stone"]
+					elseif centered then
 						local px = x - minp.x + 1
 						local pz = z - minp.z + 1
 
 						if y < avg then
 							data[ivm] = node["stone"]
-						elseif y == avg and ((px % rx) <= 5 or (pz % rz) <= 5) then
+						elseif y == avg and ((px % rx) <= streetw or (pz % rz) <= streetw) then
 							data[ivm] = node["obsidian"]
-						elseif ((px % rx) > 5 and (pz % rz) > 5) and y <= bh[math.ceil(px / rx)][math.ceil(pz / rz)] then
+						elseif y == avg then
+							data[ivm] = node["stone"]
+						elseif ((px % rx) > streetw + sidewalk and (pz % rz) > streetw + sidewalk and (px % rx) < rx - sidewalk and (pz % rz) < rz - sidewalk) and y <= bh[math.ceil(px / rx)][math.ceil(pz / rz)] then
 							if (y - avg) % 4 == 0 then
 								data[ivm] = node["stone"]
 							else
-								if px % rx <= 6 or px % rx >= rx - 1 then
+								if px % rx <= streetw + sidewalk + 1 or px % rx >= rx - (sidewalk + 1) then
 									if pz % math.floor(rz / 3) == 2 then
 										data[ivm] = node["stone"]
 									elseif pz % math.floor(rz / 3) == 5 and y <= avg + 2 then
@@ -187,7 +157,7 @@ function cityscape.generate(minp, maxp, seed)
 									else
 										data[ivm] = node["plate_glass"]
 									end
-								elseif pz % rz <= 6 or pz % rz >= rz - 1 then
+								elseif pz % rz <= streetw + sidewalk + 1 or pz % rz >= rz - (sidewalk + 1) then
 									if px % math.floor(rx / 3) == 2 then
 										data[ivm] = node["stone"]
 									elseif px % math.floor(rx / 3) == 5 and y <= avg + 2 then
