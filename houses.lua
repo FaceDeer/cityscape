ee257fe11b0aa104f0654031c5e9e92a2575a529
Duane Robertson <duane@duanerobertson.com>
2016-03-15 01:40:25 -0500

    Rework node id table.
    And add broken house materials.

--- houses.lua
+++ houses.lua
@@ -1,4 +1,5 @@
 local node = cityscape.node
+local breaker = cityscape.breaker
 local pcount = 0
 
 
@@ -15,23 +16,12 @@ local function pstore(param, x, y, z, p)
 end
 
 
-local function breaker(node)
-	if math.random(50) <= cityscape.desolation then
-		return "air"
-	elseif cityscape.desolation > 0 and math.random(10) <= cityscape.desolation then
-		return node.."_broken"
-	else
-		return node
-	end
-end
-
-
 local function crates(data, pos1, pos2)
 	local y = math.min(pos2.y, pos1.y)
 	for z = pos1.z,pos2.z do
 		for x = pos1.x,pos2.x do
-			if (data[x][y][z] == node["air"] or data[x][y][z] == nil) and math.random(1000) == 1 then
-				data[x][y][z] = node["crate"]
+			if (data[x][y][z] == node("air") or data[x][y][z] == nil) and math.random(1000) == 1 then
+				data[x][y][z] = node("cityscape:crate")
 			end
 		end
 	end
@@ -42,11 +32,11 @@ local function lights(data, param, pos1, pos2)
 	local y = math.max(pos2.y, pos1.y)
 	for z = pos1.z,pos2.z do
 		for x = pos1.x,pos2.x do
-			if (data[x][y][z] == node["air"] or data[x][y][z] == nil) and (data[x][y+1][z] == node["floor_ceiling"] or data[x][y+1][z] == node["roof"]) and math.random(20) == 1 then
+			if (data[x][y][z] == node("air") or data[x][y][z] == nil) and (data[x][y+1][z] == node("cityscape:floor_ceiling") or data[x][y+1][z] == node("cityscape:roof")) and math.random(20) == 1 then
 				if cityscape.desolation > 0 then
-					data[x][y][z] = node["light_panel_broken"]
+					data[x][y][z] = node("cityscape:light_panel_broken")
 				else
-					data[x][y][z] = node["light_panel"]
+					data[x][y][z] = node("cityscape:light_panel")
 				end
 				pstore(param, x, y, z, 20) -- 20-23
 			end
@@ -74,11 +64,11 @@ local function stairwell(data, param, pos1, pos2, left)
 				for y = 1,3 do
 					if z == 1+dz or z == 6+dz or x == 1 or x == 4 then
 						if left and x == 2 and z == 1 and y < 3 then
-							data[x + px][y + py][z + pz] = node["air"]
+							data[x + px][y + py][z + pz] = node("air")
 						elseif not left and x == 3 and z == 6+dz and y < 3 then
-							data[x + px][y + py][z + pz] = node["air"]
+							data[x + px][y + py][z + pz] = node("air")
 						else
-							data[x + px][y + py][z + pz] = node[breaker("plaster")]
+							data[x + px][y + py][z + pz] = node(breaker("cityscape:plaster"))
 						end
 					end
 				end
@@ -88,18 +78,18 @@ local function stairwell(data, param, pos1, pos2, left)
 
 	if left then
 		for i = 1,4 do
-			data[2 + px][i + py][2 + i + pz] = node["stair_stone"]
+			data[2 + px][i + py][2 + i + pz] = node("stair_stone")
 		end
 		for i = 1,3 do
-			data[2 + px][4 + py][2 + i + pz] = node["air"]
+			data[2 + px][4 + py][2 + i + pz] = node("air")
 		end
 	else
 		for i = 1,4 do
-			data[3 + px][i + py][7 - i + pz] = node["stair_stone"]
+			data[3 + px][i + py][7 - i + pz] = node("stair_stone")
 			pstore(param, 3+px, i+py, 7-i+pz, 4)
 		end
 		for i = 1,3 do
-			data[3 + px][4 + py][7 - i + pz] = node["air"]
+			data[3 + px][4 + py][7 - i + pz] = node("air")
 		end
 	end
 end
@@ -110,14 +100,14 @@ local function simple_tree(data, px, pz)
 	local r
 	local h = math.random(4,6)
 	for y = 1,h do
-		data[px][y][pz] = node["tree"]
+		data[px][y][pz] = node("tree")
 	end
 	for z = -2,2 do
 		for y = -2,2 do
 			for x = -2,2 do
 				r = math.sqrt(x ^ 2 + y ^ 2 + z ^ 2)
-				if data[x + px][y + h][z + pz] ~= node["tree"] and math.random(4,6) > r * 2 then
-					data[x + px][y + h][z + pz] = node["leaves"]
+				if data[x + px][y + h][z + pz] ~= node("tree") and math.random(4,6) > r * 2 then
+					data[x + px][y + h][z + pz] = node("leaves")
 				end
 			end
 		end
@@ -126,14 +116,15 @@ end
 
 
 local function overgrow(data, param, dx, dy, dz)
+	local sr
 	if cityscape.desolation > 0 then
 		for z = 1,dz do
 			for x = 1,dx do
 				sr = math.random(10)
 				if sr < 6 then
-					data[x][1][z] = node["grass"..sr]
+					data[x][1][z] = node("default:grass_"..sr)
 				elseif sr == 6 then
-					data[x][1][z] = node["dry_shrub"]
+					data[x][1][z] = node("default:dry_shrub")
 				end
 			end
 		end
@@ -141,10 +132,13 @@ local function overgrow(data, param, dx, dy, dz)
 end
 
 
+local function simple_schem(data, param, dx, dy, dz, mir)
+end
+
 local function simple(data, param, dx, dy, dz, mir)
 	local develop, wall_x, wall_z, floors, conc, c
 	local yard = 3
-	conc = "concrete"..math.random(2,5)
+	conc = "cityscape:concrete"..math.random(2,5)
 	overgrow(data, param, dx, dy, dz)
 
 	for z = 1+yard,dz-yard do
@@ -153,24 +147,24 @@ local function simple(data, param, dx, dy, dz, mir)
 			wall_z = z == yard + 1 or z == dz - yard
 			for y = 1,3 do
 				if wall_x and y == 2 and z == math.floor(dz / 2) then
-					data[x][y][z] = node["air"]
+					data[x][y][z] = node("air")
 				elseif wall_x and y == 1 and z == math.floor(dz / 2) then
-					data[x][y][z] = node["door"]
+					data[x][y][z] = node("doors:door_wood_b")
 					if (mir == 2) ~= (x == yard + 2) then
 						pstore(param, x, y, z, 1)
 					else
 						pstore(param, x, y, z, 3)
 					end
 				elseif (wall_x or wall_z) and y == 2 and math.random(2) == 1 then
-					data[x][y][z] = node["glass"]
+					data[x][y][z] = node(breaker("default:glass"))
 				elseif wall_x or wall_z then
-					data[x][y][z] = node["wood"]
+					data[x][y][z] = node(breaker("default:wood"))
 				elseif x > yard + 2 and x < dx - yard + 1 and z > yard + 1 and z < dz - yard then
-					data[x][y][z] = node["air"]
+					data[x][y][z] = node("air")
 				end
 			end
-			data[x][4][z] = node["roof"]
-			data[x][0][z] = node["carpet"]
+			data[x][4][z] = node(breaker("cityscape:roof"))
+			data[x][0][z] = node(breaker("cityscape:carpet"))
 		end
 	end
 
@@ -183,7 +177,7 @@ local function simple(data, param, dx, dy, dz, mir)
 	--			else
 	--				sr = math.random(30)
 	--				if sr == 1 then
-	--					data[qx * yard - 1][1][qz * yard - 1] = node["doll"]
+	--					data[qx * yard - 1][1][qz * yard - 1] = node("cityscape:doll")
 	--					pstore(param, qx * yard - 1, 1, qz * yard - 1, math.random(4) - 1)
 	--				end
 	--			end
--- init.lua
+++ init.lua
@@ -29,6 +29,34 @@ function cityscape.clone_node(name)
 	return node2
 end
 
+function cityscape.node(name)
+	if not cityscape.node_cache then
+		cityscape.node_cache = {}
+	end
+
+	if not cityscape.node_cache[name] then
+		cityscape.node_cache[name] = minetest.get_content_id(name)
+		--print("*** "..name..": "..cityscape.node_cache[name])
+		if name ~= "ignore" and cityscape.node_cache[name] == 127 then
+			print("*** Failure to find node: "..name)
+		end
+	end
+
+	return cityscape.node_cache[name]
+end
+
+function cityscape.breaker(node)
+	local sr = math.random(50)
+	if sr <= cityscape.desolation then
+		return "air"
+	elseif cityscape.desolation > 0 and sr / 5 <= cityscape.desolation then
+		return string.gsub(node, ".*:", "cityscape:").."_broken"
+	else
+		return node
+	end
+end
+
+
 dofile(cityscape.path .. "/nodes.lua")
 dofile(cityscape.path .. "/deco_rocks.lua")
 dofile(cityscape.path .. "/mapgen.lua")
